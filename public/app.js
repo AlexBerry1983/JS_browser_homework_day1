d41273ccfb25e1a426eb433af4d2ea04227f58a7
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-06-19 21:42:28 +0100

    refactored code

--- public/app.js
+++ public/app.js
@@ -1,68 +1,42 @@
-var app = function() {
+var createCatObject = function(){
   var catObject = document.createElement("ul");
   catObject.classList.add("cat");
-
+  return catObject;
+}
+var createListItem1 = function(catName){
   var listItem1 = document.createElement("li");
-  listItem1.innerText = "Name:";
-
+  listItem1.innerText = "Name: " + catName;
+  return listItem1;
+}
+var createListItem2 = function(favFood){
   var listItem2 = document.createElement("li");
-  listItem2.innerText = "Favourite food:";
-
+  listItem2.innerText = "Favourite food: " + favFood;
+  return listItem2;
+}
+var createListImg = function(imgSrc){
   var img4 = document.createElement("img");
-  img4.src = "https://upload.wikimedia.org/wikipedia/commons/c/c7/A_black_cat_named_Tilly.jpg";
-
+  img4.src = imgSrc;
+  img4.width = 500;
+  img4.height = 300;
+  return img4;
+}
+var appendElements = function(cats, catObject, listItem1, listItem2, img4){
   catObject.appendChild(listItem1);
   catObject.appendChild(listItem2);
   catObject.appendChild(img4);
+  cats.appendChild(catObject);
+}
+var addCat = function(listItem1, listItem2, imgSrc){
+  var catObject = createCatObject();
+  var listItem1 = createListItem1(listItem1);
+  var listItem2 = createListItem2(listItem2);
+  var img4 = createListImg(imgSrc)
 
   var cats = document.querySelector("#cats");
-  cats.appendChild(catObject);
+  appendElements(cats, catObject, listItem1, listItem2, img4);
+}
+var app = function() {
+  addCat("Murdock", "Whatever you're trying to have for dinner", "https://upload.wikimedia.org/wikipedia/commons/c/c7/A_black_cat_named_Tilly.jpg");
 }
 
 window.onload = app;
-
-
-
-
-
-
-
-// createListItem = function(){
-//   var listItem = document.createElement("ul");
-//   listItem.classList.add("cat");
-//   return listItem;
-// }
-// createCatName = function(catName){
-//   var catName = document.createElement("li")
-//   catName.classList.add(catName);
-//   return catName;
-// }
-// createFavouriteFood = function(favFood){
-//   var favFood = document.createElement("li")
-//   favFood.classList.add("Favourite food: " + favFood);
-//   return favFood;
-// }
-// createImage = function(image){
-//   var image = document.createElement("li")
-//   image.classList.addImage(image);
-//   return image;
-// }
-// var appendElements = function(catName, favFood, image){
-//   catName.appendChild(catName);
-//   favFood.appendChild(favFood);
-//   image.appendChild(image);
-// }
-// var addCat = function(catName, favFood, image){
-//   var listItem = createListItem();
-//   var catName = createCatName(catName);
-//   var favFood = createFavouriteFood(favFood);
-//   var image = createImage(image);
-//
-//   var quotesList = document.querySelector('#quotes');
-//   appendElements(listItem, catName, favFood, image);
-// }
-// var app = function(){
-//   addCat("Muligan", "Salmon", "cat")
-// }
-//
-// window.onload = app;
